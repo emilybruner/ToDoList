191d948b91bb964e6048544ed4945e282163a980
Emily <bruner.emilym@gmail.com>
2019-11-10 13:28:57 -0500

    task 1 complete

---
+++ my-app/src/Components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MyInfo = () => {
+    return (
+        <div>
+            <h1>Emily Bruner</h1>
+            <p>I am learning React.</p>
+            <ul>
+                <li>Fiji</li>
+                <li>South Africa</li>
+                <li>Italy</li>
+            </ul>
+        </div>
+    );
+};
+
+export default MyInfo;
